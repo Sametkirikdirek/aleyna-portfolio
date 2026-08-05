c490a99c0d9cc8fc485370cd9cbec3c8e63bbe4d
Samet Kırıkdirek <114110593+Sametkirikdirek@users.noreply.github.com>
2026-08-05 14:46:56 +0300

    feat: add blur gradient truncation in pop-up modal, font size adjustment, and dedicated /writings/:id full article page

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
 import GalleryPage from "./pages/GalleryPage";
 import WritingsPage from "./pages/WritingsPage";
+import WritingDetailPage from "./pages/WritingDetailPage";
 import AIWorkPage from "./pages/AIWorkPage";
 import ContactPage from "./pages/ContactPage";
 
@@ -26,6 +27,7 @@ function App() {
               <Route path="about" element={<AboutPage />} />
               <Route path="gallery" element={<GalleryPage />} />
               <Route path="writings" element={<WritingsPage />} />
+              <Route path="writings/:id" element={<WritingDetailPage />} />
               <Route path="ai-work" element={<AIWorkPage />} />
               <Route path="contact" element={<ContactPage />} />
             </Route>
